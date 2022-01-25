c6decef90aee1ea09a35ad94e18d2716fd233241
Fallagar <fallagar2010@gmail.com>
2022-01-25 17:50:46 +0200

    New Idea. Testing Grid and JS injections of elements

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
-const startDate = document.querySelector("#first");
-const endDate = document.querySelector("#second");
-const calculate = document.querySelector("#third");
-const result = document.querySelector("#result")
+const allChildren = document.querySelectorAll("div > div ");
+const wrDiv = document.querySelector("#wrapper");
+const wrDivChildren = document.querySelector("#wrapper").children;
 
-
-startDate.addEventListener("click", newStyle);
-endDate.addEventListener("click", newStyleTwo);
-calculate.addEventListener("click", showResultWindow)
-
-function newStyle() {
-    endDate.classList.add("endclass");
+function start(howMany) {
+    for (let n = 0; n <= howMany; n++) {
+        const para = document.createElement("div");
+        para.setAttribute("style", `color:blue;`)
+        wrDiv.appendChild(para);
+        
     }
-function newStyleTwo() {
-    calculate.classList.add("calculateClass");
+    for (let i = 0; i < wrDiv.childElementCount; i++) {
+        wrDivChildren.item(i).innerHTML = `${i}`
+    }
 }
-function showResultWindow() {
-    result.classList.add("resultClass");
-   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const startDate = document.querySelector("#first");
+// const endDate = document.querySelector("#second");
+// const calculate = document.querySelector("#third");
+// const result = document.querySelector("#result")
+
+
+// startDate.addEventListener("click", newStyle);
+// endDate.addEventListener("click", newStyleTwo);
+// calculate.addEventListener("click", showResultWindow)
+
+// function newStyle() {
+//     endDate.classList.add("endclass");
+//     }
+// function newStyleTwo() {
+//     calculate.classList.add("calculateClass");
+// }
+// function showResultWindow() {
+//     result.classList.add("resultClass");
+//    }
 // function newStyleTwo() {
 //     result.classList.add("resultClass");
 //     test.innerHTML = "You clicked ENDDATE";
